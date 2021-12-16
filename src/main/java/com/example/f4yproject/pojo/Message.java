7cd8e4271b4f2f87abe49a3afb25b96cc7388073
trabnm1313 <jame.sou.com@hotmail.com>
2021-12-16 23:53:20 +0700

    Edit POJO

--- src/main/java/com/example/f4yproject/pojo/Message.java
+++ src/main/java/com/example/f4yproject/pojo/Message.java
@@ -1,11 +1,13 @@
 package com.example.f4yproject.pojo;
 
 import com.vaadin.collaborationengine.CollaborationMessageList;
+import com.vaadin.flow.component.messages.MessageListItem;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Data
 @Document("Message")
@@ -14,9 +16,9 @@ public class Message implements Serializable {
     @Id
     String _id;
     String topic;
-    CollaborationMessageList messageList;
+    List<MessageListItem> messageList;
 
-    public Message(String _id, String topic, CollaborationMessageList messageList) {
+    public Message(String _id, String topic, List<MessageListItem> messageList) {
         this._id = _id;
         this.topic = topic;
         this.messageList = messageList;
