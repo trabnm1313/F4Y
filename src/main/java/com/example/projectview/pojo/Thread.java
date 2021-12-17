c611c8f2fc8387c3fae29e7dabc412a679d8741c
trabnm1313 <jame.sou.com@hotmail.com>
2021-12-18 00:00:45 +0700

    More pojo

---
+++ src/main/java/com/example/projectview/pojo/Thread.java
@@ -0,0 +1,36 @@
+package com.example.projectview.pojo;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Document("Thread")
+public class Thread implements Serializable {
+
+    @Id
+    String _id;
+    String ownerID;
+    String topic;
+    String message;
+    Integer like;
+    Boolean isEdited;
+    Date timeStamp;
+    String tag;
+
+    public Thread() {}
+
+    public Thread(String _id, String ownerID, String topic, String message, Integer like, Boolean isEdited, Date timeStamp, String tag) {
+        this._id = _id;
+        this.ownerID = ownerID;
+        this.topic = topic;
+        this.message = message;
+        this.like = like;
+        this.isEdited = isEdited;
+        this.timeStamp = timeStamp;
+        this.tag = tag;
+    }
+}
